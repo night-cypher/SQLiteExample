afd2360540e80decf1167e9f58b3b9bfd82f17c4
HP <krist.ng@corous360.com>
2016-05-02 20:37:45 +0800

    Initial commit

---
+++ app/src/main/java/com/example/questdot/sqliteexample/DisplayContact.java
@@ -0,0 +1,120 @@
+package com.example.questdot.sqliteexample;
+
+/**
+ * Created by HP on 2/5/2016.
+ */
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class DisplayContact extends AppCompatActivity {
+    int from_Where_I_Am_Coming = 0;
+    private DBHelper mydb ;
+
+    TextView name ;
+    TextView phone;
+    TextView email;
+    int id_To_Update = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_contact);
+        name = (TextView) findViewById(R.id.editTextName);
+        phone = (TextView) findViewById(R.id.editTextPhone);
+        email = (TextView) findViewById(R.id.editTextEmail);
+
+        mydb = new DBHelper(this);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras !=null)
+        {
+            int Value = extras.getInt("id");
+
+            if(Value>0){
+                //means this is the view part not the add contact part.
+                Cursor rs = mydb.getData(Value);
+                id_To_Update = Value;
+                rs.moveToFirst();
+
+                String nam = rs.getString(rs.getColumnIndex(DBHelper.CONTACTS_COLUMN_NAME));
+                String phon = rs.getString(rs.getColumnIndex(DBHelper.CONTACTS_COLUMN_PHONE));
+                String emai = rs.getString(rs.getColumnIndex(DBHelper.CONTACTS_COLUMN_EMAIL));
+
+
+                if (!rs.isClosed())
+                {
+                    rs.close();
+                }
+                name.setText((CharSequence)nam);
+                phone.setText((CharSequence)phon);
+                email.setText((CharSequence)emai);
+
+
+
+            }
+        }
+         }
+
+
+ public void edit(View view) {
+     Bundle extras = getIntent().getExtras();
+     if (extras != null) {
+         int Value = extras.getInt("id");
+         if (Value > 0) {
+             if (mydb.updateContact(id_To_Update, name.getText().toString(), phone.getText().toString(), email.getText().toString())) {
+                 Toast.makeText(getApplicationContext(), "Updated", Toast.LENGTH_SHORT).show();
+                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                 startActivity(intent);
+             } else {
+                 Toast.makeText(getApplicationContext(), "not Updated", Toast.LENGTH_SHORT).show();
+             }
+         } else {
+             if (mydb.insertContact(name.getText().toString(), phone.getText().toString(), email.getText().toString())) {
+                 Toast.makeText(getApplicationContext(), "done", Toast.LENGTH_SHORT).show();
+             } else {
+                 Toast.makeText(getApplicationContext(), "not done", Toast.LENGTH_SHORT).show();
+             }
+             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+             startActivity(intent);
+         }
+     }
+ }
+    public void delete(View view)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.deleteContact)
+                .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                      //  int Value = extras.getInt("id");
+                        mydb.deleteContact(id_To_Update);
+                        Toast.makeText(getApplicationContext(), "Deleted Successfully", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(intent);
+                    }
+                })
+                .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        AlertDialog d = builder.create();
+        d.setTitle("Are you sure");
+        d.show();
+    }
+
+
+}
---
+++ app/src/main/java/com/example/questdot/sqliteexample/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.questdot.sqliteexample;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    public final static String EXTRA_MESSAGE = "MESSAGE";
+    private ListView obj;
+    DBHelper mydb;
+    Button btnAdd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAdd=(Button)findViewById(R.id.btnAdd);
+
+        btnAdd.setOnClickListener(this);
+
+        mydb = new DBHelper(this);
+        ArrayList array_list = mydb.getAllCotacts();
+        ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1, array_list);
+        final ArrayList array_list2 = mydb.getid();
+
+        obj = (ListView)findViewById(R.id.listView1);
+        obj.setAdapter(arrayAdapter);
+        obj.setOnItemClickListener(new OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3) {
+                // TODO Auto-generated method stub
+
+                    int id_To_Search = Integer.parseInt(array_list2.get(arg2).toString());
+                   // Toast.makeText(getApplicationContext(),""+id_To_Search,Toast.LENGTH_LONG).show();
+                    Bundle dataBundle = new Bundle();
+                    dataBundle.putInt("id", id_To_Search);
+
+                    Intent intent = new Intent(getApplicationContext(),DisplayContact.class);
+
+                    intent.putExtras(dataBundle);
+                    startActivity(intent);
+
+
+
+            }
+        });
+    }
+
+
+     @Override
+    public void onClick(View v) {
+        if (v.equals(btnAdd)){
+            Bundle dataBundle = new Bundle();
+            dataBundle.putInt("id", 0);
+
+            Intent intent = new Intent(getApplicationContext(),DisplayContact.class);
+            intent.putExtras(dataBundle);
+
+            startActivity(intent);
+        }
+    }
+
+}
